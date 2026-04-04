33ecbfb0ff3c42657bfe10c37e6f5946557cad17
engsaadmohamed-dot <eng.saad.mohamed@gmail.com>
2026-04-04 23:04:35 +0200

    init commit

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'codenames-v1';
+const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+  )));
+});
